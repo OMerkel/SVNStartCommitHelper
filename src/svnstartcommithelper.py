4df6ea4a55b098cd5f4d887eeee59d396126752b
Oliver Merkel <Merkel.Oliver@web.de>
2012-10-15 23:21:52 +0200

    Brief: Added brief text in commit message. Multi line text fields.
    Oliver: 1. Added brief text in commit message.
    2. Comment and findings are multi line text fields w/ Scrollbars now since this has been requested a few times.
    3. os.getlogin() instead of os.environ.get('USERNAME')
    
    Findings: os.environ.get('USERNAME') did not work in some Windows environments; Got email requests that there should be a sample for multi line text with scrollbar.
    
    Reviewers(s): None
    
    Risk: Low
    
    Jira key is hlp-7
    
    Lint (Equal): ---

--- src/svnstartcommithelper.py
+++ src/svnstartcommithelper.py
@@ -86,19 +86,34 @@ class SvnStartCommitHelperView(tk.Tk):
         self.title(SvnStartCommitHelperConstants.TITLE)
         self.minsize(300,50)
 
-        tk.Label(self, text='Comment').grid(row=5, sticky=tk.E)
-        tk.Label(self, text='Findings').grid(row=10, sticky=tk.E)
+        descriptionFrame = tk.Frame(self, bd=1, relief=tk.SUNKEN)
+        descriptionFrame.grid(row=0, column=0, columnspan=3,
+            padx=2, pady=2, sticky=tk.N+tk.E+tk.S+tk.W)
+        tk.Label(descriptionFrame, text='Brief').grid(row=0, sticky=tk.E)
+        tk.Label(descriptionFrame, text='Comment').grid(row=5, sticky=tk.E)
+        tk.Label(descriptionFrame, text='Findings').grid(row=10, sticky=tk.E)
         tk.Label(self, text='Reviewer(s)').grid(row=15, sticky=tk.E)
         tk.Label(self, text='Risk').grid(row=20, sticky=tk.E)
         tk.Label(self, text='Jira key is').grid(row=25, sticky=tk.E)
         tk.Label(self, text='Lint').grid(row=30, sticky=tk.E)
 
-        self.commentText = tk.Entry(self, width=69)
-        self.commentText.grid(row=5, column=1, columnspan=2)
-        self.findingsText = tk.Entry(self, width=69)
-        self.findingsText.grid(row=10, column=1, columnspan=2)
+        self.briefText = tk.Entry(descriptionFrame, width=69)
+        self.briefText.grid(row=0, column=1, columnspan=2, sticky=tk.W+tk.E)
+        self.commentText = tk.Text(descriptionFrame, width=69, height=5, wrap=tk.WORD)
+        self.commentText.grid(row=5, column=1)
+        self.commentScrollbar = tk.Scrollbar(descriptionFrame)
+        self.commentScrollbar.grid(row=5, column=2, sticky=tk.N+tk.S)
+        self.commentScrollbar.config(command=self.commentText.yview)
+        self.commentText.config(yscrollcommand=self.commentScrollbar.set)
+        self.findingsText = tk.Text(descriptionFrame, width=69, height=5, wrap=tk.WORD)
+        self.findingsText.grid(row=10, column=1)
+        self.findingsScrollbar = tk.Scrollbar(descriptionFrame)
+        self.findingsScrollbar.grid(row=10, column=2, sticky=tk.N+tk.S)
+        self.findingsScrollbar.config(command=self.findingsText.yview)
+        self.findingsText.config(yscrollcommand=self.findingsScrollbar.set)
+
         self.reviewersText = tk.Entry(self, width=69)
-        self.reviewersText.grid(row=15, column=1, columnspan=2)
+        self.reviewersText.grid(row=15, column=1, columnspan=3, sticky=tk.W+tk.E)
         self.riskVar = tk.StringVar(self)
         self.riskVar.set(SvnStartCommitHelperConstants.NOSELECTION)
         tk.OptionMenu(self, self.riskVar, *self.OPTIONSRISK).grid(row=20, column=1)
@@ -108,16 +123,19 @@ class SvnStartCommitHelperView(tk.Tk):
         self.lintVar.set(SvnStartCommitHelperConstants.NOSELECTION)
         tk.OptionMenu(self, self.lintVar, *self.OPTIONSLINT).grid(row=30, column=1)
         self.lintText = tk.Entry(self, width=50)
-        self.lintText.grid(row=30, column=2)
+        self.lintText.grid(row=30, column=2, sticky=tk.W+tk.E)
 
-        tk.Button(self, text="OK", command=self.callback).grid(row=5, column=90, rowspan=30, sticky=tk.W+tk.E+tk.N+tk.S, padx=5, pady=5)
+        tk.Button(self, text="OK", command=self.callback).grid(row=0, column=90, rowspan=35, sticky=tk.W+tk.E+tk.N+tk.S, padx=5, pady=5)
         self.protocol('WM_DELETE_WINDOW', self.callback)
 
+    def getBriefText(self):
+        return self.briefText.get().strip()
+
     def getCommentText(self):
-        return self.commentText.get().strip()
+        return self.commentText.get('1.0', tk.END).strip()
 
     def getFindingsText(self):
-        return self.findingsText.get().strip()
+        return self.findingsText.get('1.0', tk.END).strip()
 
     def getReviewersText(self):
         return self.reviewersText.get().strip()
@@ -152,7 +170,8 @@ class SvnStartCommitHelperController(object):
 
     def writeSvnCommitMessage(self):
         f=open(tk.sys.argv[2], mode='w')
-        f.write('%s: %s\n\n' % (os.environ.get('USERNAME'), self.view.getCommentText()))
+        f.write('Brief: %s\n\n' % self.view.getBriefText())
+        f.write('%s: %s\n\n' % (os.getlogin(), self.view.getCommentText()))
         f.write('Findings: %s\n\n' % self.view.getFindingsText())
         f.write('Reviewer(s): %s\n\n' % self.view.getReviewersText())
         f.write('Risk: %s\n\n' % self.view.getRiskOption())
@@ -166,3 +185,4 @@ class SvnStartCommitHelperController(object):
 if __name__=='__main__':
     if 4==len(tk.sys.argv):
         controller = SvnStartCommitHelperController()
+
